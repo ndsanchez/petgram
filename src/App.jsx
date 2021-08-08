b19f5ccc5c18132ee2db936c7d742edd2d7eb0a6
Neil Sanchez <nd.sanchezq@gmail.com>
2021-08-08 16:17:22 -0500

    Added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ListOfCategories from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyle'
 
 const App = () => {
   return (
-    <ListOfCategories />
+    <>
+      <GlobalStyle />
+      <ListOfCategories />
+    </>
   )
 }
 
