3455b09331a988caf983bc9a438a54860c29eeac
Neil Sanchez <nd.sanchezq@gmail.com>
2021-08-08 19:35:01 -0500

    Added a logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ListOfCategories from './components/ListOfCategories'
 import ListOfPhotoCards from './components/ListOfPhotoCards'
+import Logo from './components/Logo'
 import { GlobalStyle } from './GlobalStyle'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </>
